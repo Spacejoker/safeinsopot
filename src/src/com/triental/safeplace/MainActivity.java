5aa0ca32671dcadaa7e8d97972125da57163b39a
jens <jens@sexy-top.(none)>
2012-07-05 13:27:11 +0200

    olol

--- src/src/com/triental/safeplace/MainActivity.java
+++ src/src/com/triental/safeplace/MainActivity.java
@@ -1,10 +1,16 @@
 package com.triental.safeplace;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -20,5 +26,34 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    /** Called when the user selects the Send button 
+     * @throws Throwable */
+    public void sendMessage(View view) throws Throwable {
+    	
+    	String FILENAME = "contacts";
+    	String string = "Pink skirts in sopot!";
+    	FileInputStream openFileInput = openFileInput(FILENAME);
+    	String s = convertStreamToString(openFileInput);
+    	string = s + string;
+    	openFileInput.close();
+    	
+    	FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+    	fos.write(string.getBytes());
+    	fos.close();
+
+    	openFileInput = openFileInput(FILENAME);
+    	s = "In file we now have: " + convertStreamToString(openFileInput);
+    	openFileInput.close();
+    	
+    	Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    	
+    }
     
+    String convertStreamToString(java.io.InputStream is) {
+        try {
+            return new java.util.Scanner(is).useDelimiter("\\A").next();
+        } catch (java.util.NoSuchElementException e) {
+            return "";
+        }
+    }
 }
